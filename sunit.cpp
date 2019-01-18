fd186f9a8c1d340bd9348ec60c9413be26b1012c
Neurochrom <pcichock@poczta.onet.pl>
2019-01-18 13:05:58 +0100

    Added some SFINAE based compile failure tests.

--- sunit.cpp
+++ sunit.cpp
@@ -1,14 +1,77 @@
+#include <assert.h>
 #include <iostream>
 
 #include "Unit.hpp"
 #include "UnitStream.hpp"
 
+UNIT_FULL_DEF(int, PubIndex);
+UNIT_FULL_DEF(int, PrivIndex);
+
+
+PubIndex pubi = 3;
+PubIndex pubi2 = 4;
+PrivIndex privi = 6;
+
+struct NCBase_ {};
+
+template <class T>
+struct NCDerived_ : T
+{
+   template <class C> NCDerived_(C c = C()) : T(c) {}
+   operator NCBase_() { return NCBase_(); }
+};
+
+#define ASSERT_DOES_NOT_COMPILE_PREP(a, expr, n) \
+struct NCC##n##_ \
+{ \
+   template <class A> \
+   auto f(A a) -> decltype(expr) \
+   { \
+      std::cout << "TEST FAILED: "#expr" does compile" << std::endl; \
+      assert(0 && "The compilation of this should be prevented by SFINAE if "#expr" would be illegal"); \
+      return expr; \
+   } \
+   int f(NCBase_) \
+   { \
+      std::cout << "TEST PASSED: "#expr" does not compile" << std::endl; \
+      return 0; \
+   } \
+   NCC##n##_() { f(NCDerived_<decltype(a)>(a)); } \
+}
+
+
+#define ASSERT_DOES_NOT_COMPILE_PREP2(a, b, expr, n) \
+struct NCC##n##_ \
+{ \
+   template <class A, class B> \
+   auto f(A a, B b) -> decltype(expr) \
+   { \
+      std::cout << "TEST FAILED: "#expr" does compile" << std::endl; \
+      assert(0 && "The compilation of this should be prevented by SFINAE if "#expr" would be illegal"); \
+      return expr; \
+   } \
+   int f(NCBase_, NCBase_) \
+   { \
+      std::cout << "TEST PASSED: "#expr" does not compile" << std::endl; \
+      return 0; \
+   } \
+   NCC##n##_() { f(NCDerived_<decltype(a)>(a), NCDerived_<decltype(b)>(b)); } \
+}
+
+#define ASSERT_DOES_NOT_COMPILE_EXEC(n)  NCC##n##_ nc_test##n
+
+
+ASSERT_DOES_NOT_COMPILE_PREP2(privi, pubi, privi + pubi, 0);
+ASSERT_DOES_NOT_COMPILE_PREP2(pubi, pubi2, pubi + pubi2, 1);
+ASSERT_DOES_NOT_COMPILE_PREP(pubi, pubi % pubi, 2);
+
 int main()
 {
-   std::cout << "Hi" << std::endl;
+   std::cout << "Hi " << std::endl;
 
-   UNIT_FULL_DEF(int, PubIndex);
-   UNIT_FULL_DEF(int, PrivIndex);
+   ASSERT_DOES_NOT_COMPILE_EXEC(0);
+   //ASSERT_DOES_NOT_COMPILE_EXEC(1);   // this actually does compile
+   ASSERT_DOES_NOT_COMPILE_EXEC(2);   // we have not defined operator % so this shall pass
 
    PubIndex pub = 0;
    PubIndex pub2(2);
