de5186d3966a1ee57f3eada364375f64a8cf20d0
Neurochrom <pcichock@poczta.onet.pl>
2019-01-18 13:25:42 +0100

    Moved the SFINAE based not compile test infrastructure to assertnc.hpp. Also fixed some problems with gcc and clang compilation.

---
+++ assertnc.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <typeinfo>
+
+namespace {
+
+struct NCBase_ {};
+
+template <class T>
+struct NCDerived_ : T
+{
+   template <class C> NCDerived_(C c = C()) : T(c) {}
+   operator NCBase_() { return NCBase_(); }
+};
+
+#define ASSERT_DOES_NOT_COMPILE_PREP(a, expr, n) \
+struct NCC##n##_ \
+{ \
+   template <class A> \
+   auto f(A a) -> decltype(expr) \
+   { \
+      std::cout << "TEST FAILED: "#expr" does compile" << std::endl; \
+      assert(0 && "The compilation of this should be prevented by SFINAE if "#expr" would be illegal"); \
+      return expr; \
+   } \
+   int f(NCBase_) \
+   { \
+      std::cout << "TEST PASSED: "#expr" does not compile" << std::endl; \
+      return 0; \
+   } \
+   NCC##n##_() { f(NCDerived_<decltype(a)>(a)); } \
+}
+
+
+#define ASSERT_DOES_NOT_COMPILE_PREP2(a, b, expr, n) \
+struct NCC##n##_ \
+{ \
+   template <class A, class B> \
+   auto f(A a, B b) -> decltype(expr) \
+   { \
+      std::cout << "TEST FAILED: "#expr" does compile" << std::endl; \
+      assert(0 && "The compilation of this should be prevented by SFINAE if "#expr" would be illegal"); \
+      return expr; \
+   } \
+   int f(NCBase_, NCBase_) \
+   { \
+      std::cout << "TEST PASSED: "#expr" does not compile" << std::endl; \
+      return 0; \
+   } \
+   NCC##n##_() { f(NCDerived_<decltype(a)>(a), NCDerived_<decltype(b)>(b)); } \
+}
+
+#define ASSERT_DOES_NOT_COMPILE_EXEC(n)  NCC##n##_ nc_test##n
+
+} // end namespace
